26f2e2f2f5e986d9d73a20f9167f136b87abb5dd
Администратор <danikmay33710@gmail.com>
2024-12-12 01:54:42 +0300

    JWT-аутентификация

---
+++ src/main/java/danik/may/app/SpringSecurityApplication.java
@@ -0,0 +1,21 @@
+package danik.may.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@SpringBootApplication(scanBasePackages = "danik.may")
+@ComponentScan(basePackages = {"danik.may.*"})
+@EntityScan(basePackages = {"danik.may.entity"})
+@EnableJpaRepositories("danik.may.repository")
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
