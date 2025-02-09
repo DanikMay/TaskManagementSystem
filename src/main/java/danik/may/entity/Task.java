5893056b7c7f5891a715da271df87e0c08812b47
Администратор <danikmay33710@gmail.com>
2025-02-10 00:33:57 +0300

    Сделал JavaDoc для Task Entity

--- src/main/java/danik/may/entity/Task.java
+++ src/main/java/danik/may/entity/Task.java
@@ -7,35 +7,60 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
 
+/**
+ *  Данные о задаче в системе управления задач, которые сохраняются в таблицу БД
+ */
 @Entity
 @Builder
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "tasks")
 public class Task {
+    /**
+     * Id задачи
+     */
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "task_id_seq")
     @SequenceGenerator(name = "task_id_seq", sequenceName = "task_id_seq", allocationSize = 1)
     private int id;
 
+    /**
+     * Заголовок задачи
+     */
     @Column(name = "header", nullable = false)
     private String header;
 
+    /**
+     * Описание задачи
+     */
     @Column(name = "description", nullable = false)
     private String description;
 
+    /**
+     * Статус задачи. Может быть "To Do", "Progress", "Done"
+     */
     @Column(name = "status", nullable = false)
     private String status;
 
+    /**
+     * Приоритет задачи. Может быть "Low", "Mid", "High"
+     */
     @Column(name = "priority", nullable = false)
     private String priority;
 
+    /**
+     * Username автора задачи
+     */
     @Column(name = "author", nullable = false)
     private String author;
 
+    /**
+     * Username исполнителя задачи
+     */
     @Column(name = "implementer", nullable = false)
     private String implementer;
 }
